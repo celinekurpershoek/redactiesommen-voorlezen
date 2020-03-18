08114559aaccd1597d13221a212ed2b3f108d93f
Celine Kurpershoek <celinekurpershoek@gmail.com>
2020-03-18 23:19:33 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const excersiceSelector = '#huidigeredactiesom'
+
+// Observe the excersice 
+const excersice = document.querySelector(excersiceSelector);
+const mutationObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        addSpeakButton();
+        mutationObserver.disconnect();
+    });
+});
+
+mutationObserver.observe(excersice, {
+    attributes: true,
+});
+
+// Predict based on base speechPace how long the speech will take.
+function calculateTimeout(textLenght) {
+    const speechPace = 55;
+    return textLenght * speechPace
+}
+
+// Add button to page to enable user to click it.
+function addSpeakButton() {
+    const excersice = document.querySelector(excersiceSelector)
+    const speakerButton = document.createElement('button');
+    speakerButton.setAttribute('class', 'speaker-button');
+    speakerButton.innerHTML = '🔈';
+    excersice.insertAdjacentElement('afterend', speakerButton)
+    speakerButton.addEventListener('click', speakExcersie)
+}
+
+
+// Speak with spoken library on Click
+const speakExcersie = async (e) => {
+    const speakerButton = e.target;
+    const excersice = document.querySelector(excersiceSelector).textContent;
+    const timeout = calculateTimeout(excersice.length);
+    setTimeout(() => {
+        speakerButton.removeAttribute('disabled', true)
+    }, timeout)
+    speakerButton.setAttribute('disabled', true);
+
+    (await spoken.voices())
+        .filter(voice => voice.name.indexOf('Xander') == 0)
+        .forEach(voice =>
+            spoken.say(excersice, voice.name)
+        )
+}
+
+
+
