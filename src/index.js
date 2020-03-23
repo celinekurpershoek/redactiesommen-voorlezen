89400e1ccc15044eb1491565dbc2fcaa5a385025
Celine <celinekurpershoek@gmail.com>
2020-03-23 12:00:19 +0100

    add speak button on task pages on first load; since the display wont change the addSpeaker func was not triggered

--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
 const excersiceSelector = '#huidigeredactiesom';
-
-// Observe the excersice 
 const excersice = document.querySelector(excersiceSelector);
-const mutationObserver = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        addSpeakButton();
-        mutationObserver.disconnect();
-    });
-});
-
-mutationObserver.observe(excersice, {
-    attributes: true,
-});
-
-// Predict based on base speechPace how long the speech will take.
-function calculateTimeout(textLenght) {
-    const speechPace = 55;
-    return textLenght * speechPace
-}
-
-// Add button to page to enable user to click it.
-function addSpeakButton() {
-    const excersice = document.querySelector(excersiceSelector)
-    const speakerButton = document.createElement('button');
-    speakerButton.setAttribute('class', 'speaker-button');
-    excersice.insertAdjacentElement('afterend', speakerButton)
-    speakerButton.addEventListener('click', speakExcersie)
-}
-
 
 // Speak with spoken library on Click
 const speakExcersie = async (e) => {
@@ -42,6 +14,37 @@ const speakExcersie = async (e) => {
                 speakerButton.removeAttribute('data-speaking', true)
             })
         )
+}
+
+// Observe the excersice if hidden at start
+if(excersice && excersice.getAttribute('style') ==="display: none;") {
+    const mutationObserver = new MutationObserver(function (mutations) {
+        mutations.forEach(() => {
+            addSpeakButton();
+            mutationObserver.disconnect();
+        });
+    });
+
+    mutationObserver.observe(excersice, {
+        attributes: true,
+    });
+} else if (excersice) {
+    addSpeakButton();
+} 
+
+// Predict based on base speechPace how long the speech will take.
+function calculateTimeout(textLenght) {
+    const speechPace = 55;
+    return textLenght * speechPace
+}
+
+// Add button to page to enable user to click it.
+function addSpeakButton() {
+    const excersice = document.querySelector(excersiceSelector)
+    const speakerButton = document.createElement('button');
+    speakerButton.setAttribute('class', 'speaker-button');
+    excersice.insertAdjacentElement('afterend', speakerButton);
+    speakerButton.addEventListener('click', speakExcersie);
 }
 
 // Cancel spoken on tab change or close tab
